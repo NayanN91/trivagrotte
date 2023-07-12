75fd3cdf5940abaf9ffc23bd9fdc6997ac7fbb78
Nicolas GARCIA <garcianicopro@gmail.com>
2023-07-12 22:27:28 +0200

    Navbar V1 finish

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,11 @@
+import './Footer.css';
+
+const Footer = () => {
+    return(
+        <div className='Footer-All'>
+            <img src="/src/assets/TrivaGrotte-Logo.png" alt="TrivaGrotte" />
+        </div>
+    );
+}
+
+export default Footer;
